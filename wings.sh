91722e7fe90dd754bd3d3619d73bcb8a171cd29e
123gg456 <codpro2410@gmail.com>
2025-03-10 22:12:02 +0530

    Create wings.sh

---
+++ wings.sh
@@ -0,0 +1,10 @@
+#!bin/bash
+curl -sSL https://get.docker.com/ | CHANNEL=stable bash
+sudo systemctl enable --now docker
+sudo mkdir -p /etc/pterodactyl
+curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
+sudo chmod u+x /usr/local/bin/wings
+cd /etc/pterodactyl
+wget 
+sudo systemctl enable --now wings
+echo -e "Wings Installed pls configure file in '/etc/pterodactyl/config.yml'"
