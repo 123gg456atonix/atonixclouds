37985a3b08c6e31b1d845d982cf68c7ee7815c8d
123gg456 <codpro2410@gmail.com>
2025-03-10 22:12:49 +0530

    Update wings.sh

--- wings.sh
+++ wings.sh
@@ -4,7 +4,8 @@ sudo systemctl enable --now docker
 sudo mkdir -p /etc/pterodactyl
 curl -L -o /usr/local/bin/wings "https://github.com/pterodactyl/wings/releases/latest/download/wings_linux_$([[ "$(uname -m)" == "x86_64" ]] && echo "amd64" || echo "arm64")"
 sudo chmod u+x /usr/local/bin/wings
-cd /etc/pterodactyl
+cd /etc/systemd/system/
 wget 
 sudo systemctl enable --now wings
+cd /root/
 echo -e "Wings Installed pls configure file in '/etc/pterodactyl/config.yml'"
